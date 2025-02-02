a6741fa0cbd9d06bd87d76c4f505e07e7f3829f4
nagashree7 <nagashree721@gmail.com>
2025-02-02 17:40:46 +0530

    first commit4

--- main.tf
+++ main.tf
@@ -29,15 +29,12 @@ resource "aws_security_group" "ec2_sg" {
   }
 }
 
-# EC2 Instance
 resource "aws_instance" "nginx_instance" {
   ami                    = "ami-0c55b159cbfafe1f0"  # Update with a valid Ubuntu AMI ID
   instance_type          = "t2.micro"
-  #key_name              = aws_key_pair.ec2_key.key_name
-  security_groups         = aws_security_group.ec2_sg.id
+  security_groups        = [aws_security_group.ec2_sg.name]  # FIXED: Use `.name`, not `.id`
   associate_public_ip_address = true
 
-  # Install and configure Nginx using user data
   user_data = <<-EOF
               #!/bin/bash
               apt-get update -y
@@ -50,10 +47,12 @@ resource "aws_instance" "nginx_instance" {
   tags = {
     Name = "Nginx-Server"
   }
+}
+
 
   # # Wait for the EC2 instance to be ready before applying other actions
   # wait_for_capacity_timeout = "0"
-}
+
 
 # Output the public IP of the EC2 instance
 output "instance_public_ip" {
